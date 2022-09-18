6e4bb0e97ca3cccf620e43d50696aa5c444833f3
yahiabeethoven <ybt214@nyu.edu>
2022-09-18 23:55:54 +0400

    fixed the id to name conversion

--- Week3/project1/app.js
+++ Week3/project1/app.js
@@ -1,6 +1,6 @@
 
 var extracted_teamId;
-var team_name_url = "https://app.sportdataapi.com/api/v1/soccer/teams/";
+let team_name_url = "https://app.sportdataapi.com/api/v1/soccer/teams/";
 var t_name = "";
 window.addEventListener("load", () => {
 
@@ -13,20 +13,15 @@ window.addEventListener("load", () => {
         // console.log(data.data.standings[0][0].team_id);
         
         extracted_teamId = data.data.standings[0][0].team_id;
-        console.log(extracted_teamId);
-        t_name = extracted_teamId;
-        teamName.innerHTML = t_name;
+        team_name_url += extracted_teamId;
+        team_name_url += "?apikey=3315c030-377d-11ed-b63b-53764e828d80"
+        console.log(team_name_url)
+        fetch(team_name_url)
+        .then(response => response.json())
+        .then(data => {
+            t_name = data.data.name;
+            teamName.innerHTML = t_name;
+        })
     })
-    console.log(typeof t_name)
-    team_name_url += extracted_teamId;
-    team_name_url += "?apikey=3315c030-377d-11ed-b63b-53764e828d80"
     console.log(team_name_url)
-    // team_name_url = "https://app.sportdataapi.com/api/v1/soccer/teams/" + extracted_teamId + "?apikey=3315c030-377d-11ed-b63b-53764e828d80"
-    
-    // fetch(team_name_url)
-    // .then(response => response.json())
-    // .then(data => {
-    //     t_name = data.data.name;
-    //     teamName.innerHTML = t_name;
-    // })
 })
