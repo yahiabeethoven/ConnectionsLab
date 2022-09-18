7f714aaf7abfc34a2224808fa85d590e079556ec
yahiabeethoven <ybt214@nyu.edu>
2022-09-19 02:36:41 +0400

    updated everything, but exceeded number of allowed requests for api so had to stop coding :(
    I just realized that my API expires in one day so I have to find another one.......

--- Week3/project1/app.js
+++ Week3/project1/app.js
@@ -1,33 +1,72 @@
 
 var extracted_teamId;
-var team_name_url = "https://app.sportdataapi.com/api/v1/soccer/teams/";
+var team_name_url ="";
+var team_id_url ="";
 var t_name = "";
+var teamData;
+var countryId ="";
 window.addEventListener("load", () => {
 
     console.log("Page has loaded");
     let teamName = document.getElementById("team-name");
-    // https://app.sportdataapi.com/api/v1/soccer/leagues/4?apikey=3315c030-377d-11ed-b63b-53764e828d80
-    fetch("https://app.sportdataapi.com/api/v1/soccer/standings?apikey=3315c030-377d-11ed-b63b-53764e828d80&season_id=2232")
+    fetch("https://app.sportdataapi.com/api/v1/soccer/standings?apikey=954a2ba0-37a1-11ed-95f3-3334eea78c22&season_id=2232")
     .then(response => response.json())
     .then(data => {
-        // console.log(data.data.standings[0][0].team_id);
         let cupGroups = data.data.standings;
         let groupTeams = cupGroups[0];
 
         let randomGroup = Math.floor(Math.random() * cupGroups.length);
         let randomTeam = Math.floor(Math.random() * groupTeams.length);
 
-        extracted_teamId = data.data.standings[randomGroup][randomTeam].team_id;
+        extracted_teamId = cupGroups[randomGroup][randomTeam].team_id;
+        team_name_url = "https://app.sportdataapi.com/api/v1/soccer/teams/";
         team_name_url += extracted_teamId;
-        team_name_url += "?apikey=3315c030-377d-11ed-b63b-53764e828d80";
+        team_name_url += "?apikey=954a2ba0-37a1-11ed-95f3-3334eea78c22";
         console.log(team_name_url);
         
         fetch(team_name_url)
         .then(response => response.json())
-        .then(data => {
-            t_name = data.data.name;
+        .then(info => {
+            t_name = info.data.name;
             teamName.innerHTML = t_name;
+        });
+
+        
+    });
+    let button = document.getElementById("team-button");
+        button.addEventListener("click", function() {
+            let inputText = document.getElementById("team-input").value;
+
+            let africa_url = "https://app.sportdataapi.com/api/v1/soccer/countries?apikey=954a2ba0-37a1-11ed-95f3-3334eea78c22&continent=Africa";
+            // let teamSearch;
+            
+
+            fetch(africa_url)
+            .then(response => response.json())
+            .then(continent => {
+                let teamSearch = continent.data;
+                for (var i in teamSearch) {
+                    if (teamSearch[i].name == inputText) {
+                        countryId = teamSearch[i].country_id;
+                        console.log(countryId);
+                    }
+                }
+                team_id_url = "https://app.sportdataapi.com/api/v1/soccer/teams/";
+                team_id_url += countryId;
+                team_id_url += "?apikey=954a2ba0-37a1-11ed-95f3-3334eea78c22";
+                console.log(team_id_url);
+                
+                fetch(team_id_url)
+                .then(response => response.json())
+                .then(info => {
+                    teamData = info.data;
+                    // teamName.innerHTML = t_name;
+                });
+            })
+            
+            console.log("button clicked");
+            console.log("input text is: "+inputText);
         })
-    })
-    console.log(team_name_url)
+    
+
 })
