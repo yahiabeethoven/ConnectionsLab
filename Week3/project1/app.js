deb2795ee91222400dacc78dcd13cab43b69ba0a
yahiabeethoven <ybt214@nyu.edu>
2022-09-20 17:41:58 +0400

    update page

--- Week3/project1/app.js
+++ Week3/project1/app.js
@@ -13,7 +13,7 @@ window.addEventListener("load", () => {
 
     console.log("Page has loaded");
     let teamName = document.getElementById("team-name");
-    fetch("https://app.sportdataapi.com/api/v1/soccer/standings?apikey=954a2ba0-37a1-11ed-95f3-3334eea78c22&season_id=2232")
+    fetch("https://app.sportdataapi.com/api/v1/soccer/standings?apikey=954a2ba0-37a1-11ed-95f3-3334eea78c22&season_id=2264")
     .then(response => response.json())
     .then(data => {
         let cupGroups = data.data.standings;
@@ -40,7 +40,7 @@ window.addEventListener("load", () => {
     button = document.getElementById("team-button");
         button.addEventListener("click", function() {
             inputText = document.getElementById("team-input").value;
-
+            console.log(extracted_teamId);
             africa_url = "https://app.sportdataapi.com/api/v1/soccer/countries?apikey=954a2ba0-37a1-11ed-95f3-3334eea78c22&continent=Africa";
 
             fetch(africa_url)
@@ -50,25 +50,25 @@ window.addEventListener("load", () => {
                 for (var i in teamSearch) {
                     if (teamSearch[i].name == inputText) {
                         countryId = teamSearch[i].country_id;
-                        console.log(countryId);
+                        // console.log(countryId);
                     }
                 }
                 team_id_url = "https://app.sportdataapi.com/api/v1/soccer/teams/";
                 team_id_url += countryId;
                 team_id_url += "?apikey=954a2ba0-37a1-11ed-95f3-3334eea78c22";
-                console.log(team_id_url);
+                // console.log(team_id_url);
                 
                 fetch(team_id_url)
                 .then(response => response.json())
                 .then(info => {
                     teamData = info.data;
-                    console.log(teamData);
+                    // console.log(teamData);
                     // teamName.innerHTML = t_name;
                 });
             })
             
-            console.log("button clicked");
-            console.log("input text is: "+inputText);
+            // console.log("button clicked");
+            // console.log("input text is: "+inputText);
         })
     
 
