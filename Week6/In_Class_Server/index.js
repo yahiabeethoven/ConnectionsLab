9ed7cff589929c7d75fa6a260d17109126edfdac
yahiabeethoven <ybt214@nyu.edu>
2022-10-04 11:11:45 +0400

    finalized server

--- Week6/In_Class_Server/index.js
+++ Week6/In_Class_Server/index.js
@@ -2,7 +2,11 @@ let express = require("express");
 let app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.send("Hello from my own server!");
+    let obj = {
+        "apples" : 10
+    };
+    res.json(obj);
 })
 
 app.listen(3000, () => {
